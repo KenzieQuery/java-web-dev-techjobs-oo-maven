8b53bd629805f827c42433f9c831706f85e42659
KenzieQuery <william_query@comcast.com>
2021-07-08 15:02:51 -0600

    adding commetns and improving readability

--- src/main/java/com/company/JobField.java
+++ src/main/java/com/company/JobField.java
@@ -10,32 +10,27 @@ public abstract class JobField {
         this.id = nextId;
         nextId++;
     }
-
     public JobField(String value) {
         this.value = value;
     }
-
+    //Methods equals, hashCode, and toString
     @Override
     public abstract boolean equals(Object o);
-
     @Override
     public int hashCode() {
         return Objects.hash(id);
     }
-
     @Override
     public String toString() {
         return this.value;
     }
-
+    //getters and setters
     public int getId() {
         return id;
     }
-
     public String getValue() {
         return value;
     }
-
     public void setValue(String value) {
         this.value = value;
     }
